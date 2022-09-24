77b292e7e6c5c6e8e5995f1163281772651f5c39
Odi000 <mandimandi.ro@gmail.com>
2022-09-24 18:02:59 +0200

    Done.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const button=document.querySelector('button');
 
-button.addEventListener('click', () => console.log(getComputerChoice(1,3)));
-
-const usrChoice = prompt("Rock, Paper, Scissors, shoot!","");
-const pcChoice = getComputerChoice(1,3);
+button.addEventListener('click', () => game());
 
 //This functiont generates a random choice for the PC
 function getComputerChoice(min,max){
@@ -22,25 +19,52 @@ function getComputerChoice(min,max){
 
 //This is a single round of the game function
 function playRound(usrSelection, pcSelection){
-    if(usrSelection.toLowerCase() === pcSelection.toLowerCase()){
-        return console.log(`Draw! You both chose ${pcSelection}`);
-    } switch(true){
-        case usrSelection.toLowerCase() === 'rock' && pcSelection.toLowerCase() === 'paper':
-            return console.log('You Lost! Paper beats Rock')
-            break;
-        case usrSelection.toLowerCase() === 'rock' && pcSelection.toLowerCase() === 'scissors':
-            return console.log('You Won! Rock beats Scissors')
-            break;
-        case usrSelection.toLowerCase() === 'paper' && pcSelection.toLowerCase() === 'rock':
-            return console.log('You Won! Paper beats Rock')
-            break;
-        case usrSelection.toLowerCase() === 'paper' && pcSelection.toLowerCase() === 'scissors':
-            return console.log('You Lost! Scissors beats Paper')
-            break;
-        case usrSelection.toLowerCase() === 'scissors' && pcSelection.toLowerCase() === 'rock':
-            return console.log('You Lost! Rock beats Scissors')
-            break;
-        case usrSelection.toLowerCase() === 'scissors' && pcSelection.toLowerCase() === 'paper':
-            return console.log('You Won! Scissors beats Paper')
+    if(Boolean(usrSelection)){
+        if(usrSelection.toLowerCase() === pcSelection.toLowerCase()){
+            return console.log(`Draw! You both chose ${pcSelection}`), 'draw';
+        } switch(true){
+            case usrSelection.toLowerCase() === 'rock' && pcSelection.toLowerCase() === 'paper':
+                return console.log('You Lost! Paper beats Rock'), false;
+                break;
+            case usrSelection.toLowerCase() === 'rock' && pcSelection.toLowerCase() === 'scissors':
+                return console.log('You Won! Rock beats Scissors'), true;
+                break;
+            case usrSelection.toLowerCase() === 'paper' && pcSelection.toLowerCase() === 'rock':
+                return console.log('You Won! Paper beats Rock'), true;
+                break;
+            case usrSelection.toLowerCase() === 'paper' && pcSelection.toLowerCase() === 'scissors':
+                return console.log('You Lost! Scissors beats Paper'), false;
+                break;
+            case usrSelection.toLowerCase() === 'scissors' && pcSelection.toLowerCase() === 'rock':
+                return console.log('You Lost! Rock beats Scissors'), false;
+                break;
+            case usrSelection.toLowerCase() === 'scissors' && pcSelection.toLowerCase() === 'paper':
+                return console.log('You Won! Scissors beats Paper'), true;
+        }
+    }else alert('Round Missed!\nYou should write Rock, Paper or Scissors to play.');
+}
+
+//Run the game function
+function game(){
+    let score = 0;
+
+    for(let i=0; i<5; i++){
+        const usrChoice = prompt("Rock, Paper, Scissors, shoot!","");
+        const pcChoice = getComputerChoice(1,3);
+        const result = playRound(usrChoice,pcChoice);
+
+        if(result==='draw'){
+            score+=0.5;
+            console.log(`Score: ${score}/5`);
+        }else if(result){
+            score++
+            console.log(`Score: ${score}/5`);
+        }else{
+            console.log(`Score: ${score}/5`);
+        }
     }
+    //Using tenerary operators just for fun
+    score > 2.5? console.log('Congratz, You Won!'):
+    score == 2.5? console.log('Wow it\'s a draw! Never happened before.'):
+    console.log("You lost my firend, but don't worry life goes on.\nPeople have seen worse days.");
 }
