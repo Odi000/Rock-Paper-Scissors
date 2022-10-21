5c6e23aaada62cf565c017bc463d1d33ea8d0f8e
Odi000 <mandimandi.ro@gmail.com>
2022-10-22 00:14:23 +0200

    styled the buttons, added the score counter and round results

--- script.js
+++ script.js
@@ -1,21 +1,68 @@
+//First screen
 const container = document.querySelector('div.container');
 const playBtn = document.querySelector('button');
 const title = document.querySelector('.container>h2');
 
 //The stage after you click the play button
-const gamePlay = document.createElement('div'),
+
+//Buttons
+const buttonsContainer = document.createElement('div'),
     rockBtn = document.createElement('button'),
     paperBtn = document.createElement('button'),
-    scissorsBtn = document.createElement('button');
+    scissorsBtn = document.createElement('button'),
+    rockImg = document.createElement('img'),
+    paperImg = document.createElement('img'),
+    scissorsImg = document.createElement('img');
 
+rockBtn.appendChild(rockImg);
+paperBtn.appendChild(paperImg);
+scissorsBtn.appendChild(scissorsImg);
+buttonsContainer.appendChild(rockBtn);
+buttonsContainer.appendChild(paperBtn);
+buttonsContainer.appendChild(scissorsBtn);
+
+//Game-Play div
+const gamePlay = document.createElement('div'),
+    divUsr = document.createElement('div'),
+    divResult = document.createElement('div'),
+    divPc = document.createElement('div'),
+    scoreDispUsr = document.createElement('h1'),
+    scoreDispPc = document.createElement('h1'),
+    winImg = document.createElement('img'),
+    loseImg = document.createElement('img'),
+    drawImg = document.createElement('img');
+
+divUsr.textContent = 'Player Score:';
+divPc.textContent = 'Computer Score:';
+scoreDispUsr.textContent = 0;
+scoreDispPc.textContent = 0;
+
+divUsr.appendChild(scoreDispUsr);
+divPc.appendChild(scoreDispPc);
+gamePlay.appendChild(divUsr);
+gamePlay.appendChild(divResult);
+gamePlay.appendChild(divPc);
+
+
+//Classes&Names
+buttonsContainer.classList.add('buttons-container');
 gamePlay.classList.add('game-play');
-rockBtn.classList.add('rock');
-paperBtn.classList.add('paper');
-scissorsBtn.classList.add('scissors');
+rockBtn.classList.add('select');
+paperBtn.classList.add('select');
+scissorsBtn.classList.add('select');
+divResult.classList.add('result');
 
-rockBtn.textContent = 'Rock';
-paperBtn.textContent = 'Paper';
-scissorsBtn.textContent = 'Scissors';
+rockBtn.name = 'rock';
+paperBtn.name = 'paper';
+scissorsBtn.name = 'scissors';
+
+//Img sources
+rockImg.src = './Images/stone.png';
+paperImg.src = './Images/new-document.png';
+scissorsImg.src = './Images/scissors.png';
+winImg.src = './Images/check.png'
+loseImg.src = './Images/cancel.png'
+drawImg.src = './Images/balance.png'
 
 playBtn.addEventListener('click', () => game());
 
@@ -24,53 +71,56 @@ function getComputerChoice(min,max){
     const rndNumber = Math.floor(Math.random()*(max-min+1)+min);
     switch(rndNumber){
         case 1:
-            return 'Rock';
-            break;
+            return 'rock';
         case 2:
-            return 'Paper';
-            break;
+            return 'paper';
         case 3:
-            return 'Scissors';
+            return 'scissors';
     }
 }
 
-//This is a single round of the game function
-function playRound(usrSelection, pcSelection){
-    if(!usrSelection) return;
-    
-    const usrSel = usrSelection.toLowerCase(),
-          pcSel = pcSelection.toLowerCase();
-
-
-        if(usrSel === pcSel){
-            return console.log(`Draw! You both chose ${pcSelection}`), 0;
-        } switch(true){
-            case usrSel === 'rock' && pcSel === 'paper':
-                return console.log('You Lost! Paper beats Rock'),
-                -1;
-                break;
-            case usrSel === 'rock' && pcSel === 'scissors':
-                return console.log('You Won! Rock beats Scissors'),
-                1;
-                break;
-            case usrSel === 'paper' && pcSel === 'rock':
-                return console.log('You Won! Paper beats Rock'),
-                1;
-                break;
-            case usrSel === 'paper' && pcSel === 'scissors':
-                return console.log('You Lost! Scissors beats Paper'),
-                -1;
-                break;
-            case usrSel === 'scissors' && pcSel === 'rock':
-                return console.log('You Lost! Rock beats Scissors'),
-                -1;
-                break;
-            case usrSel === 'scissors' && pcSel === 'paper':
-                return console.log('You Won! Scissors beats Paper'),
-                1;
-            default:
-                return alert('Round Missed!\nYou should write Rock, Paper or Scissors to play.');
+function getRoundWinner(usrChoice,pcChoice){
+    if(usrChoice === pcChoice){
+        return 0;
+    } switch(true){
+        case usrChoice === 'rock' && pcChoice === 'paper':
+            return -1;            
+        case usrChoice === 'rock' && pcChoice === 'scissors':
+            return 1;
+        case usrChoice === 'paper' && pcChoice === 'rock':
+            return 1;
+        case usrChoice === 'paper' && pcChoice === 'scissors':
+            return -1;
+        case usrChoice === 'scissors' && pcChoice === 'rock':
+            return -1;
+        case usrChoice === 'scissors' && pcChoice === 'paper':
+            return 1;
         }
+}
+
+//This is a single round of the game function
+function playRound(){
+    const usrChoice = this.name;
+    const pcChoice = getComputerChoice(1,3);
+    const result = getRoundWinner(usrChoice,pcChoice);
+    let playerScore = parseInt(scoreDispUsr.textContent);
+    let pcScore = parseInt(scoreDispPc.textContent);
+    
+    divResult.innerHTML = '';
+
+    if(result=== 1) {
+        scoreDispUsr.textContent = ++playerScore;
+        divResult.appendChild(winImg);
+    }else if(result === -1) {
+        scoreDispPc.textContent = ++pcScore;
+        divResult.appendChild(loseImg);
+    }else{
+        divResult.appendChild(drawImg);
+    }
+
+    if(playerScore === 5 || pcScore === 5) {
+        console.log('u ban pes pik');
+    }
 }
 
 //Run the game function
@@ -78,23 +128,13 @@ function game(){
     container.removeChild(playBtn);
     container.removeChild(title);
 
-    container.appendChild(rockBtn);
-    container.appendChild(paperBtn);
-    container.appendChild(scissorsBtn);
+    container.appendChild(buttonsContainer);
+    container.appendChild(gamePlay);
 
-    let playerScore = 0, pcScore = 0;
-
-    // while(playerScore < 5 && pcScore < 5){
-    //     // const usrChoice = prompt("Rock, Paper, Scissors, shoot!","");
-    //     const pcChoice = getComputerChoice(1,3);
-    //     const result = playRound(usrChoice,pcChoice);
-
-    //     if(result=== 1) playerScore++;
-    //     if(result === -1) pcScore++
-
-    //     console.log(`My Score:${playerScore} | PC Score:${pcScore}`);
-    // }
-    //Using tenerary operators just to not forget that they exist
-    playerScore > pcScore? console.log('Congratz, You Won!'):
-    console.log("You lost my firend, but don't worry life goes on.\nPeople have seen worse days.");
+    const buttons = document.querySelectorAll('.select');
+    buttons.forEach(button => button.addEventListener('click', playRound))
 }
+
+drawImg.addEventListener('load', () => {
+    console.log('u loadingu');
+})
